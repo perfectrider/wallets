4fe9649c993e901fa82ea8a8d8702a3a8eced70c
perfectrider <konsgrebenev@gmail.com>
2023-02-27 18:34:40 +0300

    urls, admin created. started create views and serializers

--- bank/wallets/admin.py
+++ bank/wallets/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_on')
+
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'name', 'type', 'currency', 'balance', 'created_on', 'modified_on')
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'receiver', 'transfer_amount', 'timestamp')
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Wallet, WalletAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+
+
--- bank/wallets/views.py
+++ bank/wallets/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from wallets.models import Wallet
+from wallets.serializers import WalletSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Wallet.objects.all()
+        serializer = WalletSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = WalletSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

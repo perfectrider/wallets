326d1590a2a9dee5135f14a8fb958042b89f8d0d
perfectrider <konsgrebenev@gmail.com>
2023-02-27 16:15:41 +0300

    1st var models created

--- bank/wallets/models.py
+++ bank/wallets/models.py
@@ -1,20 +1,48 @@
 from django.db import models
+from django.conf import settings
 
-class BankUser(models.Model):
-    pass
+
+class Customer(models.Model):
+    '''User ob bank application'''
+
+    name = models.CharField(max_length='50')
+    created_on = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Wallet(models.Model):
+    '''Customer wallet. Each customer cannot create more than 5 wallets'''
+
+    CARD_TYPE_CHOICES = [
+        'Visa',
+        'MasterCard'
+    ]
+
+    CURRENCY_CHOICES = [
+        ('RUB', 'Ruble'),
+        ('USD', 'Dollar'),
+        ('EUR', 'Euro'),
+
+    ]
+
+    owner = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name='wallets')
     name = models.CharField()
-    type = models.Choices()
-    currency = models.Choices()
-    balance = models.IntegerField()
+    type = models.CharField(max_length=10, choices=CARD_TYPE_CHOICES)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
+    balance = models.DecimalField(default=0, max_digits=12, decimal_places=2)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        f'{self.owner.name} wallet - {self.name}'
+
+
 class Transaction(models.Model):
-    sender = models.ForeignKey(BankUser)
-    receiver = models.ForeignKey(BankUser)
-    transfer_amount = models.IntegerField()
-    commission = models.IntegerField()
-    status = models.Choices()
+    '''Describes departure and destination points, amount of transaction and some details.'''
+
+    sender = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='from_wallet')
+    receiver = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='to_wallet')
+    transfer_amount = models.DecimalField(max_digits=12, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)

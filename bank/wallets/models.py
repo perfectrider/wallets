fbb0d09759f038a403ba7a440beb0f84c93ca829
perfectrider <konsgrebenev@gmail.com>
2023-02-27 12:39:26 +0300

    models layout created

--- bank/wallets/models.py
+++ bank/wallets/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class BankUser(models.Model):
+    pass
+
+class Wallet(models.Model):
+    name = models.CharField()
+    type = models.Choices()
+    currency = models.Choices()
+    balance = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+class Transaction(models.Model):
+    sender = models.ForeignKey(BankUser)
+    reciever = models.ForeignKey(BankUser)
+    transfer_amount = models.IntegerField()
+    comission = models.IntegerField()
+    status = models.Choices()
+    timestamp = models.DateTimeField(auto_now_add=True)

35987fa6633b62741af899259ecd705956a9a3a2
perfectrider <konsgrebenev@gmail.com>
2023-02-27 13:08:24 +0300

    gen for wallet name created

---
+++ bank/wallets/generators/walletname.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def NameGen():
+    symbols = string.ascii_uppercase + string.digits
+    rand_name = ''.join(random.sample(symbols, 8))
+    return rand_name
+
+print(NameGen())

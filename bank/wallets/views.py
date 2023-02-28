90a30d8b2677d7c1043b5082da8f70b16369bc31
perfectrider <konsgrebenev@gmail.com>
2023-02-28 12:02:56 +0300

    views with GenericAPIView and Mixins

--- bank/wallets/views.py
+++ bank/wallets/views.py
@@ -5,49 +5,70 @@ from wallets.models import Wallet
 from wallets.serializers import WalletSerializer
 from rest_framework.views import APIView
 from django.http import Http404
+from rest_framework import mixins, generics
 
-# class WalletsViewSet(viewsets.ModelViewSet):
-#     queryset = Wallet.objects.all()
-#     serializer_class = WalletSerializer
 
-class WalletsList(APIView):
-    '''List of all wallets. Create new wallet'''
+# ----- Realization with APIView -----
+#
+# class WalletsList(APIView):
+#     '''List of all wallets. Create new wallet'''
+#
+#     def get(self, request, format=None):
+#         wallets = Wallet.objects.all()
+#         serializer = WalletSerializer(wallets, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = WalletSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+# class WalletDetail(APIView):
+#     '''Retrieve, update or delete wallet'''
+#
+#     def get_object(self, pk):
+#         try:
+#             return Wallet.objects.get(pk=pk)
+#         except Wallet.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk, format=None):
+#         wallet = self.get_object(pk)
+#         serializer = WalletSerializer(wallet)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         wallet = self.get_object(pk)
+#         serializer = WalletSerializer(wallet, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         wallet = self.get_object(pk)
+#         wallet.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def get(self, request, format=None):
-        wallets = Wallet.objects.all()
-        serializer = WalletSerializer(wallets, many=True)
-        return Response(serializer.data)
+# --------------------------
 
-    def post(self, request, format=None):
-        serializer = WalletSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# ----- Mixins Realization -----
 
-class WalletDetail(APIView):
-    '''Retrieve, update or delete wallet'''
+class WalletsList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    '''There is a using of GenericAPIView in this class,
+    and we add List and Create models mixins here.'''
 
-    def get_object(self, pk):
-        try:
-            return Wallet.objects.get(pk=pk)
-        except Wallet.DoesNotExist:
-            raise Http404
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
 
-    def get(self, request, pk, format=None):
-        wallet = self.get_object(pk)
-        serializer = WalletSerializer(wallet)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        wallet = self.get_object(pk)
-        serializer = WalletSerializer(wallet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        wallet = self.get_object(pk)
-        wallet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# ---------------------------
